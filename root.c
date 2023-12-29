awesomeWM/awesome

client: Rewrite the client stacking code in Lua.
This adds 2 new requests:

 * request::raise: Helps to decouple ewmh and client class
    internally plus allow some focus stealing policies.
 * reqest::restack: Send *why* something is restacked to Lua. It
    allows to do things like sending a client to the back of a
    layout rather than use the master area.

This is mostly a 1:1 port of the C code. The idea is to use this as
a starting point to have a stack per `tag` rather than something
global. It also paves the way for stacking wibox among clients
in a predictable way. None of that is exposed in the public API
as part of this commit. The point is to get enough going so the
wibox desktop layer PR can be implemented on top of this.

root.c
@@ -42,6 +42,7 @@
 #include "objects/button.h"
 #include "common/luaclass.h"
 #include "xwindow.h"
+#include "stack.h"
 
 #include "math.h"
 
@@ -653,6 +654,7 @@ const struct luaL_Reg awesome_root_methods[] =
     { "tags", luaA_root_tags },
     { "__index", luaA_root_index },
     { "__newindex", luaA_root_newindex },
+    { "set_stacking_order", luaA_set_stacking_order},
     { "set_index_miss_handler", luaA_root_set_index_miss_handler},
     { "set_call_handler", luaA_root_set_call_handler},
     { "set_newindex_miss_handler", luaA_root_set_newindex_miss_handler},

Elv13/awesome-1

root.c
@@ -42,6 +42,7 @@
 #include "objects/button.h"
 #include "common/luaclass.h"
 #include "xwindow.h"
+#include "stack.h"
 
 #include "math.h"
 
@@ -653,6 +654,7 @@ const struct luaL_Reg awesome_root_methods[] =
     { "tags", luaA_root_tags },
     { "__index", luaA_root_index },
     { "__newindex", luaA_root_newindex },
+    { "_set_stacking_order", luaA_set_stacking_order},
     { "set_index_miss_handler", luaA_root_set_index_miss_handler},
     { "set_call_handler", luaA_root_set_call_handler},
     { "set_newindex_miss_handler", luaA_root_set_newindex_miss_handler},